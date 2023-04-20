b9b0b2114dc5f8de71085507d05d30667ce6349e
Pedro <pedrolaraburu@gmail.com>
2023-04-20 11:17:51 -0300

    feat: add send first message endpoint

--- routers/router.py
+++ routers/router.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException
-from core.schemas.messageSchema import *
+from core.schemas.messageSchema import Message
 from core.database.database import db
 from datetime import *
 
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -18,29 +19,34 @@ async def get_thread_by_id(id: str):
     raise HTTPException(status_code=404, detail="Thread not found")
 
 
+@app.post("/send_first_message")
+async def send_first_message(sender_id: str, receiver_id: str, messageData: Message):
+    nThread = {
+        "participants": [sender_id, receiver_id],
+        "messages": [
+            {
+                "content": messageData.text,
+                "created_at": datetime.strftime(datetime.utcnow(), "%Y-%m-%d %H:%M:%S"),
+                "sender_id": sender_id,
+                "receiver_id": receiver_id
+            },
+        ]
+    }
+    db.insert(nThread)
+    return {'message': 'Message sent sucessfully'}
+
+
 @app.post("/send_message")
-async def send_message(sender_id: str, receiver_id: str, messageData: Message):
-    if (receiver_id > sender_id):
-        thread = db.fetch({"participants?contains": sender_id, "participants?contains": receiver_id})
-    elif (sender_id == receiver_id):
-        raise HTTPException(status_code=400, detail="Sender and receiver IDs cannot be the same")
-    else: 
-        thread = db.fetch({"participants?contains": receiver_id, "participants?contains": sender_id})
+async def send_message(thread_id: str, sender_id: str, messageData: Message):
+    thread = db.fetch({"key": thread_id})
     if not thread.items:
-        nThread = {
-            "participants": [sender_id, receiver_id],
-            "messages": [
-                {
-                    "content": messageData.text,
-                    "created_at": datetime.strftime(datetime.utcnow(), "%Y-%m-%d %H:%M:%S"),
-                    "sender_id": sender_id,
-                    "receiver_id": receiver_id
-                },
-            ]
-        }
-        db.insert(nThread)
-        return {'message': 'Message sent sucessfully'}
-    message_doc =  {
+        raise HTTPException(status_code=404, detail="Thread not found")
+    for x in thread.items:
+        for message in x['messages']:
+            if sender_id != message['receiver_id']:
+                receiver_id = message['receiver_id']
+                break
+    message_doc = {
         "content": messageData.text,
         "created_at": datetime.strftime(datetime.utcnow(), "%Y-%m-%d %H:%M:%S"),
         "sender_id": sender_id,
@@ -51,4 +57,3 @@ async def send_message(sender_id: str, receiver_id: str, messageData: Message):
         key = i['key']
     db.update(append_message, key=key)
     return {'message': 'Message sent successfully'}
-
