7ae92bba7023615824bb3863e2b3bb063d32d908
Pedro <pedrolaraburu@gmail.com>
2023-04-27 17:55:12 -0300

    feat: add routes to main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import messages, problem
+from routers import messages, reports
 
 app = FastAPI()
 
 # Routes
-app.include_router(problem.router, tags=(["Problemas"]), prefix="/problemas")
-app.include_router(messages.router, tags=(["Mensagens"]), prefix="/mensagens")
+app.include_router(messages.router, tags=(["Mensagens"]), prefix="/messages")
+app.include_router(reports.router, tags=(["Problemas"]), prefix="/reports")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
