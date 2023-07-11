661e46734a151195269d7ebe2c73ae679a05ba17
Pedro <pedrolaraburu@gmail.com>
2023-07-11 20:26:12 -0300

    feat: add jwt token

--- src/core/repositories/user_repository.py
+++ src/core/repositories/user_repository.py
@@ -1,19 +1,26 @@
+from fastapi import HTTPException, status
 from src.core.schemas import schemas
 from src.core.models.models import User
 from src.core.dto.dto import CreateUserOutput
 from src.core.utils.utils import value_exists, selectValue, selectEverything
 from src.core.errors.errors import DuplicateEntryError, DatabaseError, NotFoundError
-from src.core.database.hash_passwords import hash_password
-from datetime import datetime
+from src.core.database.hash_passwords import hash_password, password_verify
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
 import uuid
 from sqlalchemy.ext.asyncio import AsyncSession
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
 
 
 class UserRepository:
     def __init__(self, db: AsyncSession) -> None:
         self.db = db
 
-    async def create(self, userSchema: schemas.User): #Create an user
+    # Create an user
+    async def create(self, userSchema: schemas.User):
         async with self.db as session:
             userExists = await value_exists(self.db, User, User.email, userSchema.email)
             if userExists:
@@ -60,20 +67,65 @@ class UserRepository:
             raise DatabaseError(
                 f"Errir when inserting user data into the database: {str(error)}")
 
-    async def get(self, userId: str): # Function to get the user
+    # Function to get the user
+    async def get(self, userId: str):
         user = await selectValue(self.db, User, User.id, userId)
         if not user:
             raise NotFoundError('User not found')
         return user
 
-    async def getViaEmail(self, userEmail: str): # Function to get the user by inserting the email
+    # Function to get the user by the email
+    async def get_by_email(self, userEmail: str):
         user = await selectValue(self.db, User, User.email, userEmail)
         if not user:
             raise NotFoundError('User not found')
         return user
 
-    async def select_everything(self): # SELECT * from users
+    # SELECT * from users
+    async def select_everything(self):
         user = await selectEverything(self.db, User)
         if not user:
             raise NotFoundError('Something went wrong')
         return user
+
+    # Function that generates the jwt token
+    async def user_login(self, userEmail: str, passwordU: str, expires_in: int = (24 * 60 * 7)):
+        user = await selectValue(self.db, User, User.email, userEmail)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail='User or password are wrong')
+        else:
+            if password_verify(password=passwordU, hash_password=user.senha):
+                exp = datetime.utcnow() + timedelta(minutes=expires_in)
+                payload = {
+                    'sub': userEmail,
+                    'exp': exp
+                }
+                access_token = jwt.encode(
+                    payload, key=SECRET_KEY, algorithm=ALGORITHM)
+                return {
+                    'access_token': access_token,
+                    'exp': exp.isoformat()
+                }
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, detail='User or password are wrong')
+
+    # Function to see if the token is valid
+    async def verify_token(self, access_token):
+        try:
+            data = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        except JWTError:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid access token'
+            )
+
+        user_on_db = await selectValue(self.db, User, User.email, data['sub'])
+
+        if user_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Invalid access token'
+            )
+        return user_on_db
